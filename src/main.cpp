13eda484cd3f4aee41edbbd2714536d81254777b
Jannik1020 <jannik.thiele.official@gmail.com>
2025-05-11 18:31:23 +0200

    renamed sprites and displaying it

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,10 @@ int main()
     auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "Bomberman");
     window.setFramerateLimit(144);
 
+    sf::Texture texture("src/sprites/gnome_blue.png");
+    sf::Sprite sprite(texture);
+    sprite.setScale(sf::Vector2f(10,10));
+
     while (window.isOpen())
     {
         while (const std::optional event = window.pollEvent())
@@ -16,6 +20,7 @@ int main()
         }
 
         window.clear();
+        window.draw(sprite);
         window.display();
     }
 }
